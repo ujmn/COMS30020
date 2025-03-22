385623a76cbf212da9f48c14d8181f41b24c7af8
vic-lib <vic_cheung@126.com>
2025-03-22 18:44:16 +0800

    inintial

---
+++ src/RedNoise.cpp
@@ -0,0 +1,64 @@
+#include <CanvasTriangle.h>
+#include <DrawingWindow.h>
+#include <Utils.h>
+#include <fstream>
+#include <vector>
+
+#define WIDTH 320
+#define HEIGHT 240
+
+void draw(DrawingWindow &window) {
+	window.clearPixels();
+	// std::cout << "draw" << std::endl;
+	for (size_t y = 0; y < window.height; y++) {
+		for (size_t x = 0; x < window.width; x++) {
+			float red = rand() % 256;
+			float green = 0.0;
+			float blue = 0.0;
+			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+			window.setPixelColour(x, y, colour);
+		}
+	}
+}
+
+void handleEvent(SDL_Event event, DrawingWindow &window) {
+	if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
+		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
+		window.savePPM("output.ppm");
+		window.saveBMP("output.bmp");
+	}
+}
+
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
+	std::vector<float> ret;
+	float num = (to - from) / (numberOfValues - 1);
+	float tmp = from;
+	for (int i = 0; i < numberOfValues; i++) {
+		ret.emplace_back(tmp);
+		tmp += num;
+	}
+
+	return ret;
+}
+
+int main(int argc, char *argv[]) {
+	// DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+	// SDL_Event event;
+	// while (true) {
+	// 	// We MUST poll for events - otherwise the window will freeze !
+	// 	if (window.pollForInputEvents(event)) handleEvent(event, window);
+	// 	draw(window);
+	// 	// Need to render the frame at the end, or nothing actually gets shown on the screen !
+	// 	window.renderFrame();
+	// }
+	std::vector<float> result;
+	result = interpolateSingleFloats(2.2, 8.5, 7);
+	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+	std::cout << std::endl;
+
+	return 0;
+}
